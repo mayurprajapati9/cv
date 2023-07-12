bb1e7a4966366d0ea3f6dba950efc8d5ae7e535c
Mathakiya Mayurbhai Bharatbhai <123148601+mayurprajapati9@users.noreply.github.com>
2023-07-12 22:33:21 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+
+<!-- Index.html Page -->
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Mayur's Personal Site</title>
+
+  <style>
+
+    body {
+      background-color: #EAF6F6;
+    }
+
+    hr {
+      background-color: white;
+    }
+
+
+  </style>
+
+  </head>
+
+  <!-- The Site of Mayur Prajapati -->
+  <body>
+    <table cellspacing="20">
+      <tr>
+        <td><img src="mayurprajapati.jpg" alt="Mayur Prajapati Profile picture" width="100" height="100"></td>
+    <!--  <td><img src="C:\Users\mayur\Desktop\WEB\image\mayurprajapati.jpg" alt="Mayur Prajapati Profile picture" width="100" heigh="100"></td> -->
+        <td><h1>Mayur Prajapati</h1>
+          <p><em>Web Developer,<strong>Ethical Heaker</strong>.</em></p>
+            <p>I am Mayur Prajapati, i was study in <em><a href="https://ksv.ac.in/" target="_blank">Kadi Sarva Vidhyalaya </u></a></em> in the department of <strong>Master of Science in Information Technology</strong> i am complete the Post gredguate Program in 2024 and after completion i will like to work in the IT company as an intern t gain the industrial experience and improve my skills and knowledge</p></td>
+      </tr>
+    </table>
+    <hr size="2" noshade>
+    <!-- Education -->
+    <h3>Education</h3>
+    <ul>
+      <li>Shree Maneklal M. Patel Institute of Sciece& Resarch</li>
+        <ul>
+          <li>Msater of Science in Information Technology</li>
+        </ul>
+      <li>Shree swamivivekanand College</li>
+      <ul>
+        <li>Bechlor of Computer Application</li>
+      </ul>
+    </ul>
+    <hr>
+    <!-- Work Experience -->
+    <h3>Work Experience</h3>
+    <table cellspacing="10">
+      <thead>
+        <tr>
+          <th>Dates</th>
+          <th>Works</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>2019-2020</td>
+          <td>Video Editing on Kamo Comady Oficial YouTube Chanale</td>
+        </tr>
+        <tr>
+          <td>2021</td>
+          <td>The Project of Online News Portal Website in My Graduation</td>
+        </tr>
+      </tbody>
+    </table>
+    <hr>
+    <!-- Skills -->
+    <h3>Skills</h3>
+    <table  cellspacing="10">
+      <tbody>
+        <tr>
+          <td>Video Editing</td><td>⭐⭐⭐</td>
+        </tr>
+        <tr>
+          <td>Linux Operator</td><td>⭐⭐</td>
+        </tr>
+        <tr>
+          <td>Web Development</td><td>⭐⭐⭐⭐</td>
+        </tr>
+        <tr>
+          <td>Computer Network& Hardware</td><td>⭐⭐</td>
+        </tr>
+      </tbody>
+    </table>
+    <hr>
+    <!-- Other Page Links -->
+  <a href="hobbies.html">My Hobbies</a><br>
+  <a href="contect.html">Contect Me</a><br>
+  </body>
+</html>
